adc0ae03b4442489f4690573aad0b0f19fe12e8b
Bailym <bm39@hotmail.co.uk>
2020-11-20 14:29:29 +0000

    modify server to use port 443 (http)

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 443
 
 
 
 //routing
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/api/hello', (req, res) => res.send('Hello World!'))
 
 
 
