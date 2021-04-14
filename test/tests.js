8c9e59a17415c52245b9ddc5a187c70a063768c5
Bailym <bm39@hotmail.co.uk>
2021-04-14 14:50:29 +0100

    add test for user login

--- test/tests.js
+++ test/tests.js
@@ -10,7 +10,7 @@ suite("INTEGRATION TESTS", async function () {
 
     var newUserID = null
 
-    test("Test Login Path /api/register/:email/:password/:firstname/:surname", async function () {
+    test("Test Register Path /api/register/:email/:password/:firstname/:surname", async function () {
 
         //Register a new user with the following credentials 
         await agent.post(`/api/register/test@email.co.uk/testpassword123!/Api/Test`)
@@ -20,23 +20,31 @@ suite("INTEGRATION TESTS", async function () {
             })
 
 
-         //Check the new user exists in the users table
+        //Check the new user exists in the users table
         await agent.get(`/api/users/${newUserID}`)
             .then(function (response, error) {
                 chai.assert.equal(response.body[0].ID, newUserID, "User Not Registered!")
                 chai.assert.equal(response.body[0].FirstName, "Api", "User Not Registered!")
                 chai.assert.equal(response.body[0].Surname, "Test", "User Not Registered!")
                 chai.assert.equal(response.body[0].Email, "test@email.co.uk", "User Not Registered!")
-            }) 
+            })
     })
 
+    test("Test Login Path /api/login/:email/:password", async function () {
 
-    teardown("Suite Teardown - Remove any Created Entities", async function(){
+        //Log the user in
+        await agent.post(`/api/login/test@email.co.uk/testpassword123!`)
+            .then(function (response, error) {
+                chai.assert.equal(response.body, true, "Error Logging in User!")
+            })
+    })
+
+    suiteTeardown("Suite Teardown - Remove any Created Entities", async function () {
 
         await agent.post(`/api/users/delete/${newUserID}`)
             .then(function (response, error) {
                 chai.assert.equal(response.statusCode, 200, "Error Deleting User")
-            }) 
+            })
 
     })
 })
