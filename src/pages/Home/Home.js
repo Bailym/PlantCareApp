f5cf227e958f0383cfc6a0ad235f2c313bf0c5b6
Bailym <bm39@hotmail.co.uk>
2022-08-22 12:41:24 +0100

    remove requirement to be logged in to see home screen

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import 'antd/dist/antd.css';
 import "./Home.css";
 import PlantSearch from "../../components/PlantSearch/PlantSearch";
@@ -16,6 +16,7 @@ function Home() {
   const [gardenPreviewComponents, setGardenPreviewComponents] = useState([]);
   const [loading, setLoading] = useState(true);
   const [rssComponents, setRssComponents] = useState([]);
+  const [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
 
@@ -39,23 +40,29 @@ function Home() {
   useEffect(() => {
     //useEffect cannot be async so I have declared a function and called it inside useEffect. This is OK.
     async function getGardenPreviewComponents() {
-      let plantList = [];
-      let currentItem = [];
 
-      //create a list of plants
-      for (var i = 0; i < gardenData.length; i++) {
-        currentItem = await axios.get(`/api/plants/${gardenData[i].PlantID}`);
-        plantList.push(currentItem.data[0])
+      let components = []
+
+      if (loggedIn) {
+        let plantList = [];
+        let currentItem = [];
+
+        //create a list of plants
+        for (var i = 0; i < gardenData.length; i++) {
+          currentItem = await axios.get(`/api/plants/${gardenData[i].PlantID}`);
+          plantList.push(currentItem.data[0])
+        }
+
+        //map the plants to components
+        components = plantList.map(x =>
+          <List.Item key={x.PlantID}>
+            <Typography.Text><Button type="dashed" onClick={() => goToPlant(x.PlantID)} style={{ backgroundColor: "#f5e8cb" }}>
+              {x.CommonName}</Button></Typography.Text>
+          </List.Item>)
       }
-
-      let components = [];
-
-      //map the plants to components
-      components = plantList.map(x =>
-        <List.Item key={x.PlantID}>
-          <Typography.Text><Button type="dashed" onClick={() => goToPlant(x.PlantID)} style={{ backgroundColor: "#f5e8cb" }}>
-            {x.CommonName}</Button></Typography.Text>
-        </List.Item>)
+      else {
+        components.push(<div>Please <Link to="/login">log in</Link> to save plants to your garden</div>)
+      }
       setGardenPreviewComponents(components); //these will render on next update
     }
     getGardenPreviewComponents();
@@ -109,11 +116,10 @@ function Home() {
   async function checkUser() {
     await axios.get('/api/checkuser')  //call the server endpoint
       .then(async response => {
-        if (response.data === false) {   //if false redirect to login (you are not logged in.)
-          history.push("/login");
-        }
-        //if a user is logged in
-        else {
+        setLoggedIn(response.data); //set the logged in state
+
+        //if the user is logged in
+        if (response.data) {
           // Check the user Type (Admin/Standard) to see which home page to show
           await axios.get('/api/usertype')
             .then(response => {
@@ -127,7 +133,7 @@ function Home() {
 
   return (
     <Spin id="spin-container" spinning={loading}>
-      <PlantSearch id="search"  />
+      <PlantSearch id="search" />
       <div id="home-row">
         <div className="home-col" >
           <Card title="My Garden">
